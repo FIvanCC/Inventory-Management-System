77950397ca445bb12b7e5690dfa06f7a0a82ca4a
FIvanCC <1155152116@link.cuhk.edu.hk>
2024-02-22 12:09:45 +0800

    [server] Create API endpoints for getting ELabels and getting Elabel by name

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 app.use(cors());
+app.use(express.json());
 
 const ELabel = require('./models/ELabelModel');
 const mongoose = require('mongoose');
@@ -13,19 +14,51 @@ db.on('error', console.error.bind(console, 'Connection error:'));
 // Upon opening the database successfully
 db.once('open', function () {
     console.log("Connection is open...");
-    const newELabel = new ELabel({
-        name: 'Example Name',
-        price: 100,
-        quantity: 20
+    // const newELabel = new ELabel({
+    //     name: 'Apple juice',
+    //     price: 10,
+    //     quantity: 5
+    // });
+
+    // newELabel.save()
+    //     .then((doc) => {
+    //         console.log('Document saved:', doc);
+    //     })
+    //     .catch((err) => {
+    //         console.error('Error saving document:', err);
+    //     });
+
+    //get all the elabels
+    app.get('/getELabels/', (req, res) => {
+        ELabel.find({}, "-_id -__v")
+            .then((data) => {
+                console.log('Data:', data);
+                res.send(data);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+                res.status(500).send(error);
+            });
     });
 
-    newELabel.save()
-        .then((doc) => {
-            console.log('Document saved:', doc);
-        })
-        .catch((err) => {
-            console.error('Error saving document:', err);
-        });
+    app.get('/getELabel/:name', (req, res) => {
+        const name = req.params.name;
+
+        ELabel.findOne({ name: name }, "-_id -__v")
+            .then((data) => {
+                if (data) {
+                    console.log('Data:', data);
+                    res.send(data);
+                } else {
+                    res.status(404).send({ message: 'ELabel not found' });
+                }
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+                res.status(500).send(error);
+            });
+    });
+
 
     app.all('/*', (req, res) => {
         res.send('Hello World!');
